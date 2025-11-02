92e4e4c9b66b82b41f2d1b6d288aa04fa13a5339
rubyst <markovoiq@outlook.com>
2025-11-02 17:20:02 +0300

    Add activerecord and create posts table

--- app.rb
+++ app.rb
@@ -2,9 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
 
 
 set :database, {adapter: "sqlite3", database: "leprosorium.db"}
+
+class Post < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Leprosorium"
